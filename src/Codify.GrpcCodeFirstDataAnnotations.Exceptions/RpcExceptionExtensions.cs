582e2c83e39c347497956459ae6cc27481ae7944
Andrew Van Stokkum <AndrewVanStokkum@co-operativebank.co.nz>
2026-05-11 09:28:46 +1200

    Simplify trailer serialization

--- src/Codify.GrpcCodeFirstDataAnnotations.Exceptions/RpcExceptionExtensions.cs
+++ src/Codify.GrpcCodeFirstDataAnnotations.Exceptions/RpcExceptionExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
 using Grpc.Core;
 
@@ -14,13 +12,8 @@ public static class RpcExceptionExtensions
     public static IList<DataAnnotationValidationTrailers> GetValidationErrors(this RpcException exception)
     {
         var validationTrailer = exception.Trailers.FirstOrDefault(x => x.Key == TrailerKey);
-        return validationTrailer?.Value.FromBase64<IList<DataAnnotationValidationTrailers>>();
-    }
-
-    private static T FromBase64<T>(this string base64)
-    {
-        var bytes = Convert.FromBase64String(base64);
-        var json = Encoding.Default.GetString(bytes);
-        return JsonSerializer.Deserialize<T>(json);
+        return validationTrailer != null
+            ? JsonSerializer.Deserialize<IList<DataAnnotationValidationTrailers>>(validationTrailer.Value)
+            : new List<DataAnnotationValidationTrailers>();
     }
 }
--- src/Codify.GrpcCodeFirstDataAnnotations/Internal/ValidationResultExtensions.cs
+++ src/Codify.GrpcCodeFirstDataAnnotations/Internal/ValidationResultExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.Json;
 using Codify.GrpcCodeFirstDataAnnotations.Exceptions;
 using Grpc.Core;
@@ -13,17 +10,8 @@ internal static class ValidationResultExtensions
     public static Metadata ToValidationMetadata(this IList<DataAnnotationValidationTrailers> trailers)
     {
         var metadata = new Metadata();
-        if (trailers.Any())
-        {
-            metadata.Add(new Metadata.Entry(RpcExceptionExtensions.TrailerKey, ToBase64(trailers)));
-        }
-        return metadata;
-    }
-
-    internal static string ToBase64(IList<DataAnnotationValidationTrailers> trailers)
-    {
         var json = JsonSerializer.Serialize(trailers);
-        var bytes = Encoding.Default.GetBytes(json);
-        return Convert.ToBase64String(bytes);
+        metadata.Add(new Metadata.Entry(RpcExceptionExtensions.TrailerKey, json));
+        return metadata;
     }
 }
